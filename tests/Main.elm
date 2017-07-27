3df2a677b53b57bff156231c8966525e04065ee9
Thibaut Gery <thibaut.gery@gmail.com>
2017-07-27 16:50:43 +1000

    import test dependecies even is not used

--- tests/Main.elm
+++ tests/Main.elm
@@ -3,7 +3,7 @@ port module Main exposing (..)
 import Sample.Generated
 import Sample.String
 import Todo.Update.Delete
---import Todo.Update.Check
+import Todo.Update.Check
 import Test exposing(Test, describe)
 import Test.Runner.Node exposing (run, TestProgram)
 import Json.Encode exposing (Value)
